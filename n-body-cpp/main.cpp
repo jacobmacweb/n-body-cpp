1460f5d23e76cad4a6a40caffcca51d3ceb4b26d
Jacob MacKenzie-Websdale <jacobmacweb@gmail.com>
2023-12-03 17:56:21 +0000

    Initial Commit

---
+++ n-body-cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  n-body-cpp
+//
+//  Created by Jacob MacKenzie-Websdale on 03/12/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
